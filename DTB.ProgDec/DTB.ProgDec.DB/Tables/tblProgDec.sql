8ffaf3ddb128befcfafcc0730acb6b0782d694be
Daniel Butzen <300057079@fvtc.edu>
2020-09-12 16:24:53 -0400

    table creation

---
+++ DTB.ProgDec/DTB.ProgDec.DB/Tables/tblProgDec.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[tblProgDec]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [ProgramId] INT NOT NULL, 
+    [StudentId] INT NOT NULL, 
+    [ChangeDate] DATETIME NOT NULL
+)
---
+++ DTB.ProgDec/DTB.ProgDec.DB/Tables/tblStudent.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[tblStudent]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [FirstName] VARCHAR(30) NOT NULL, 
+    [LastName] VARCHAR(30) NOT NULL, 
+    [StudentId] VARCHAR(10) NOT NULL
+)
