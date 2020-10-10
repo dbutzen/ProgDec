f3b3820da0cfda2b7f71947c62a58cd9854adcf1
Daniel Butzen <300057079@fvtc.edu>
2020-10-10 12:50:57 -0400

    thru wk 7

---
+++ DTB.ProgDec/DTB.ProgDec.WFUI/MaintainDegreeTypes.aspx
@@ -0,0 +1,41 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="MaintainDegreeTypes.aspx.cs" Inherits="DTB.ProgDec.WFUI.MaintainDegreeTypes" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+    <div class="header rounded-top">
+        <h3>Degree Types</h3>
+    </div>
+    <p>
+
+    </p>
+    <div class="form-row ml-2 mt-2">
+        <div class="control-label col-md-2">
+            <asp:Label ID="lblDegreeTypePick" runat="server" Text="Degree Types:" >
+
+            </asp:Label>
+        </div>
+        <div class="control-label col-md-3">
+            <asp:DropDownList ID="ddlDegreeTypes" runat="server" AutoPostBack="true" OnSelectedIndexChanged="ddlDegreeTypes_SelectedIndexChanged" >
+
+            </asp:DropDownList>
+        </div>
+    </div>
+
+    <div class="form-row ml-2 mt-2">
+        <div class="control-label col-md-2">
+            <asp:Label ID="Label1" runat="server" Text="Descriptipn:">
+
+            </asp:Label>
+        </div>
+        <div class="control-label col-md-3">
+            <asp:TextBox ID="txtDescription" runat="server">
+
+            </asp:TextBox>
+        </div>
+
+        <div class="form-group mt-2 ml-5">
+            <asp:Button ID="btnInsert" runat="server" CssClass="btn btn-primary btn-lg ml-3" Text="Insert" OnClick="btnInsert_Click"/>
+            <asp:Button ID="btnUpdate" runat="server" CssClass="btn btn-primary btn-lg ml-3" Text="Update" OnClick="btnUpdate_Click"/>
+            <asp:Button ID="btnDelete" runat="server" CssClass="btn btn-primary btn-lg ml-3" Text="Delete" OnClick="btnDelete_Click"/>
+        </div>
+    </div>
+
+</asp:Content>
---
+++ DTB.ProgDec/DTB.ProgDec.WFUI/MaintainDegreeTypes.aspx.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using DTB.ProgDec.BL;
+using DTB.ProgDec.BL.Models;
+
+namespace DTB.ProgDec.WFUI
+{
+    public partial class MaintainDegreeTypes : System.Web.UI.Page
+    {
+        List<DegreeType> degreeTypes;
+        DegreeType degreeType;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            //If not postback (here for the first time)
+
+            if (!IsPostBack)
+            {
+                // Call the correct load method in the BL
+                degreeTypes = new List<DegreeType>();
+                degreeTypes = DegreeTypeManager.Load();
+                Rebind();
+
+                // put into session
+                Session["degreetypes"] = degreeTypes;
+            }
+            else
+            {
+                // Load from session
+                degreeTypes = (List<DegreeType>)Session["degreetypes"];
+            }
+
+        }
+
+        private void Rebind()
+        {
+            // Bind the list to the dropdownlist
+            ddlDegreeTypes.DataSource = null;
+            ddlDegreeTypes.DataSource = degreeTypes;
+
+            // Designate the display field
+            ddlDegreeTypes.DataTextField = "Description";
+
+            // Designate the primary key field
+            ddlDegreeTypes.DataValueField = "Id";
+
+            // Refresh the binding
+            ddlDegreeTypes.DataBind();
+
+            txtDescription.Text = string.Empty;
+        }
+
+        protected void btnInsert_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                degreeType = new DegreeType();
+                // Get typed description from screen
+                degreeType.Description = txtDescription.Text;
+
+                // Add to database
+                DegreeTypeManager.Insert(degreeType);
+
+                // Add to list
+                degreeTypes.Add(degreeType);
+                Rebind();
+
+
+            }
+            catch (Exception ex)
+            {
+
+                Response.Write("Error: " + ex.Message);
+            }
+        }
+
+        protected void btnUpdate_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int index = ddlDegreeTypes.SelectedIndex;
+                // Get the one the user selected
+                degreeType = degreeTypes[ddlDegreeTypes.SelectedIndex];
+                // Get typed description from screen
+                degreeType.Description = txtDescription.Text;
+
+                // Add to database
+                DegreeTypeManager.Update(degreeType);
+
+                // Update the list
+                degreeTypes[ddlDegreeTypes.SelectedIndex] = degreeType;
+                Rebind();
+
+
+                //ddlDegreeTypes.SelectedIndex = index;
+                ddlDegreeTypes_SelectedIndexChanged(sender, e);
+
+
+            }
+            catch (Exception ex)
+            {
+
+                Response.Write("Error: " + ex.Message);
+            }
+        }
+
+        protected void btnDelete_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Get the one the user selected
+                degreeType = degreeTypes[ddlDegreeTypes.SelectedIndex];
+
+                // Add to database
+                DegreeTypeManager.Delete(degreeType.Id);
+
+                // Update the list
+                degreeTypes.Remove(degreeType);
+                Rebind();
+
+
+            }
+            catch (Exception ex)
+            {
+
+                Response.Write("Error: " + ex.Message);
+            }
+        }
+
+        protected void ddlDegreeTypes_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            // GEt selected degree type
+            degreeType = degreeTypes[ddlDegreeTypes.SelectedIndex];
+
+            //Display
+            txtDescription.Text = degreeType.Description;
+        }
+    }
+}
